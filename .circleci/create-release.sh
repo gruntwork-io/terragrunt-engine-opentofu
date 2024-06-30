6fd01d34dc95240e184b2621f5b41c18ddde82fe
Denis O <denis.o@linux.com>
2024-06-30 17:36:01 +0100

    Separate step to upload assets

--- .circleci/create-release.sh
+++ .circleci/create-release.sh
@@ -16,7 +16,9 @@ $COMMITS
 
 EOF
 
-cd "${RELEASE}"
 gh release create "${VERSION}" \
-  -F ../release_notes.txt \
-  -t "Release ${VERSION}" *
+  -F release_notes.txt \
+  -t "Release ${VERSION}"
+
+# Upload the release assets
+upload-github-release-assets --git-tag "${VERSION}" release/*
