78f021b6155a916d4376f40c3ec9381e487ce39e
Denis O <denis.o@linux.com>
2024-07-01 20:22:00 +0100

    Add step to create release

--- .circleci/create-release.sh
+++ .circleci/create-release.sh
@@ -2,12 +2,24 @@
 # Create Github release from release candidate tag
 set -euo pipefail
 set -x
+RC_VERSION=${TAG}
 VERSION=${TAG%-rc*}
 RELEASE="${RELEASE:-release}"
 export GH_TOKEN=${GW_GITHUB_OAUTH_TOKEN}
 
 COMMITS=$(git log $(git describe --tags --abbrev=0 @^)..@ --pretty=format:"- %s")
 
+cat << EOF > rc_release_notes.txt
+## Release $RC_VERSION
+
+### Changes
+$COMMITS
+
+EOF
+# create release candidate pre-release
+gh release create "${RC_VERSION}" --prerelease -F rc_release_notes.txt -t "${RC_VERSION}" release/*
+
+# create release
 cat << EOF > release_notes.txt
 ## Release $VERSION
 
@@ -16,4 +28,4 @@ $COMMITS
 
 EOF
 
-gh release create "${VERSION}" -F release_notes.txt -t "${VERSION}" release/*
+
