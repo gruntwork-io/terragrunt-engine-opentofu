29d8ca61664ff28b36f587c0a407efb8e8302b2f
Denis O <denis.o@linux.com>
2024-07-01 20:26:00 +0100

    Assets downloading

--- .circleci/create-release.sh
+++ .circleci/create-release.sh
@@ -2,12 +2,13 @@
 # Create Github release from release candidate tag
 set -euo pipefail
 set -x
-RC_VERSION=${TAG}
-VERSION=${TAG%-rc*}
-RELEASE="${RELEASE:-release}"
-export GH_TOKEN=${GW_GITHUB_OAUTH_TOKEN}
+readonly  RC_VERSION=${TAG}
+readonly VERSION=${TAG%-rc*}
+readonly RELEASE="${RELEASE:-release}"
+readonly  GH_TOKEN=${GW_GITHUB_OAUTH_TOKEN}
 
-COMMITS=$(git log $(git describe --tags --abbrev=0 @^)..@ --pretty=format:"- %s")
+readonly COMMITS=$(git log $(git describe --tags --abbrev=0 @^)..@ --pretty=format:"- %s")
+
 
 cat << EOF > rc_release_notes.txt
 ## Release $RC_VERSION
@@ -19,6 +20,8 @@ EOF
 # create release candidate pre-release
 gh release create "${RC_VERSION}" --prerelease -F rc_release_notes.txt -t "${RC_VERSION}" release/*
 
+# download pre-release files to folder release-bin to be used in release
+
 # create release
 cat << EOF > release_notes.txt
 ## Release $VERSION
@@ -28,4 +31,9 @@ $COMMITS
 
 EOF
 
+function main {
+}
 
+main "$@"
+
+}
